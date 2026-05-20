defb706e732aa4ca5f928fc80fb976548c303269
ViniciusCandido-x <vc052285@gmail.com>
2026-05-20 08:46:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,52 @@
-const CACHE_NAME = 'vault-ledger-v1.4-landing-splash-logo';
+const CACHE_NAME = 'vault-ledger-v2.3-pwa-mobile';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './vault-ledger-icon-192.png',
+  './vault-ledger-icon-512.png',
+  './favicon.png',
+  './splash-logo.png'
+];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './vault-ledger-icon-192.png',
-      './vault-ledger-icon-512.png',
-      './favicon.png',
-      './splash-logo.png'
-    ]).catch(() => null))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => null));
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+  const request = event.request;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy)).catch(() => null);
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request).then((response) => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => null);
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => null);
         return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
   );
 });
